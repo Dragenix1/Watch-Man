8d153af53a91cb2cd4d4caea751404a790578d60
Robin <robin.jestaedt@web.de>
2023-07-07 23:38:05 +0200

    First Enemy Movement

---
+++ Assets/script/Robin/EnemyMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 3.5f;
+    private NavMeshAgent agent;
+    private Rigidbody rb;
+    [SerializeField] private Transform[] targets;
+    private Transform lastTarget;
+    private Transform newTarget;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        lastTarget = targets[Random.Range(0, targets.Length - 1)];
+        agent.SetDestination(lastTarget.position);
+        agent.speed = speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject != lastTarget.gameObject) return;
+
+        do
+        {
+            newTarget = targets[Random.Range(0, targets.Length - 1)];
+        } while (newTarget == lastTarget);
+
+        lastTarget = newTarget;
+        agent.SetDestination(lastTarget.position);
+    }
+}

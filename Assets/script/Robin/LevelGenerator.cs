938f113b684be9fb56571ce2458641a7459dd877
Robin <robin.jestaedt@web.de>
2023-07-08 12:17:34 +0200

    Level Generator v0.1.2
    added navMesh baking, added prefabs

--- Assets/script/Robin/LevelGenerator.cs
+++ Assets/script/Robin/LevelGenerator.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -28,10 +29,19 @@ public class LevelGenerator : MonoBehaviour
     private Level[,] level;
     private List<int> possiblePositions = new List<int>();
 
+    private NavMeshSurface surface;
+    private GameObject baseLevel;
+    private Transform baseLevelTransform;
     [SerializeField] int maxObstacleSize = 5;
     [SerializeField] int midObstacleSize = 2;
     [SerializeField] int smallObstacleSize = 1;
 
+    private void Awake()
+    {
+        surface = GetComponent<NavMeshSurface>();
+        baseLevel = Instantiate(new GameObject("baseLevel"), Vector3.zero, Quaternion.identity);
+        baseLevelTransform = baseLevel.transform;
+    }
     void Start()
     {
         if (size.x <= 5) size.x = 5;
@@ -51,6 +61,8 @@ public class LevelGenerator : MonoBehaviour
                 GenerateOneLine(i);
             }
         }
+
+        surface.BuildNavMesh();
     }
 
     private void GenerateStart()
@@ -91,7 +103,7 @@ public class LevelGenerator : MonoBehaviour
                 {
                     if (i != -1 && i != maxObstacleSize)
                     {
-                        Instantiate(smallObstacle[Random.Range(0, smallObstacle.Length)], new Vector3((position + 2 - maxObstacleSize + i) * 10, 0.5f * 10, lineNumber * 10), Quaternion.Euler(0, Random.Range(0, 4) * 90f, 0));
+                        Instantiate(smallObstacle[Random.Range(0, smallObstacle.Length)], new Vector3((position + 2 - maxObstacleSize + i) * 10, 10, lineNumber * 10), Quaternion.Euler(0, Random.Range(0, 4) * 90f, 0));
                     }
                     possiblePositions.Remove(position - i);
                 }
@@ -127,7 +139,7 @@ public class LevelGenerator : MonoBehaviour
                 {
                     if (i != leftDisplacement + 1)
                     {
-                        Instantiate(smallObstacle[Random.Range(0, smallObstacle.Length)], new Vector3((position + 1 - i) * 10, 0.5f * 10, lineNumber * 10), Quaternion.identity);
+                        Instantiate(smallObstacle[Random.Range(0, smallObstacle.Length)], new Vector3((position + 1 - i) * 10, 10, lineNumber * 10), Quaternion.identity);
                     }
                     possiblePositions.Remove(position - i);
                 }
@@ -135,7 +147,7 @@ public class LevelGenerator : MonoBehaviour
                 {
                     if(i != obstacleSize - leftDisplacement)
                     {
-                        Instantiate(smallObstacle[Random.Range(0, smallObstacle.Length)], new Vector3((position + 1 + i) * 10, 0.5f * 10, lineNumber * 10), Quaternion.identity);
+                        Instantiate(smallObstacle[Random.Range(0, smallObstacle.Length)], new Vector3((position + 1 + i) * 10, 10, lineNumber * 10), Quaternion.identity);
                     }
                     possiblePositions.Remove(position + i);
                 }
@@ -157,20 +169,20 @@ public class LevelGenerator : MonoBehaviour
         {
             for (int j = 0; j < size.y; j++)
             {
-                newObject = Instantiate(plane, new Vector3(i * 10, 0, j * 10), Quaternion.identity);
+                newObject = Instantiate(plane, new Vector3(i * 10, 0, j * 10), Quaternion.identity, baseLevelTransform);
             }
         }
 
         for (int i = 0; i <= size.x - 1; i++)
         {
-            Instantiate(wall, new Vector3(i * 10, 0.5f * 10, 0), Quaternion.identity);
+            Instantiate(wall, new Vector3(i * 10, 25, 0), Quaternion.identity, baseLevelTransform);
             //level[i, 0].isFilled = true;
             //level[i, 0].fill = Fill.Wall;
             //level[i, 1].isFilled = true;
             //level[i, 1].fill = Fill.Start;
             //level[i, 2].isFilled = true;
             //level[i, 2].fill = Fill.Start;
-            Instantiate(wall, new Vector3(i * 10, 0.5f * 10, (size.y - 1) * 10), Quaternion.identity);
+            Instantiate(wall, new Vector3(i * 10, 25, (size.y - 1) * 10), Quaternion.identity, baseLevelTransform);
             //level[i, size.y - 1].isFilled = true;
             //level[i, size.y - 1].fill = Fill.Wall;
             //level[i, size.y - 2].isFilled = true;
@@ -181,10 +193,10 @@ public class LevelGenerator : MonoBehaviour
 
         for (int i = 1; i < size.y - 1; i++)
         {
-            Instantiate(wall, new Vector3(0, 0.5f * 10, i * 10), Quaternion.identity);
+            Instantiate(wall, new Vector3(0, 25, i * 10), Quaternion.identity, baseLevelTransform);
             //level[0, size.y - 1].isFilled = true;
             //level[0, size.y - 1].fill = Fill.Wall;
-            Instantiate(wall, new Vector3((size.x - 1) * 10, 0.5f * 10, i * 10), Quaternion.identity);
+            Instantiate(wall, new Vector3((size.x - 1) * 10, 25, i * 10), Quaternion.identity, baseLevelTransform);
             //level[size.x - 1, size.y - 1].isFilled = true;
             //level[size.x - 1, size.y - 1].fill = Fill.Wall;
         }

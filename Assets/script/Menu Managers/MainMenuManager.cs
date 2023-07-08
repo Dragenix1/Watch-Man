78ed3fd89e9ac2d2a1c7c41f4023da86c41662a3
Eric <erichaas21@gmail.com>
2023-07-08 02:23:16 +0200

    Basic Escape & Options Menu with working sound options
    Sound options are the only options implemented until now. Available in both main menu and game scene but doesnt save set values (will probably fix with scriptable objects)

--- Assets/script/Menu Managers/MainMenuManager.cs
+++ Assets/script/Menu Managers/MainMenuManager.cs
@@ -5,9 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject optionsMenu;
+
+    private void Start()
+    {
+        optionsMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (optionsMenu.activeInHierarchy && Input.GetKeyDown(KeyCode.Escape))
+        {
+            optionsMenu.SetActive(false);
+        }
+    }
+
     public void OnPlayClick()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void OnOptionClick()
+    {
+        if (optionsMenu.activeInHierarchy)
+        {
+            optionsMenu.SetActive(false);
+        }
+        else
+        {
+            optionsMenu.SetActive(true);
+        }
     }
 
     public void OnExitClick()
